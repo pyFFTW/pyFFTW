4cae74284d696183e48a3af0c5f1bcc4dd4cff52
Frederik Beaujean <Frederik.Beaujean@lmu.de>
2014-11-17 11:44:36 +0100

    [setup] Fix error output redirection, include checker into custom_build_ext

--- setup.py
+++ setup.py
@@ -44,25 +44,14 @@ finally:
 
 version = _version.version
 
+# todo infos still printed, even with error threshold
 # 0=minimal output, 2=maximum debug
-log.set_verbosity(2)
+# log.set_verbosity(0)
+log.set_threshold(log.ERROR)
 
-# todo check latest cython doc at sage server on best practice for
-#      building. I think cythonize is the way to go; it seems to
-#      be only way to pass macros from setup.py into cython for
-#      conditional compilation
-
-try:
-    from Cython.Distutils import build_ext as build_ext
-    sources = [os.path.join(os.getcwd(), 'pyfftw', 'pyfftw.pyx')]
-except ImportError as e:
-    sources = [os.path.join(os.getcwd(), 'pyfftw', 'pyfftw.c')]
-    if not os.path.exists(sources[0]):
-        raise ImportError(str(e) + '. ' +
-                'Cython is required to build the initial .c file.')
-
-    # We can't cythonize, but that's ok as it's been done already.
-    from distutils.command.build_ext import build_ext
+# we require cython to know which part of wrapper to build depending on available libraries
+from Cython.Distutils import build_ext as build_ext
+sources = [os.path.join(os.getcwd(), 'pyfftw', 'pyfftw.pyx')]
 
 # check what's available
 import contextlib
@@ -70,7 +59,7 @@ import contextlib
 @contextlib.contextmanager
 def stdchannel_redirected(stdchannel, dest_filename):
     """
-    A context manager to temporarily redirect stdout or stderr
+    A context manager to temporarily redirect stdout and stderr to a file.
 
     e.g.:
 
@@ -80,7 +69,7 @@ def stdchannel_redirected(stdchannel, dest_filename):
 
     Taken from http://stackoverflow.com/a/17752729/987623
     """
-
+    dest_file = None
     try:
         oldstdchannel = os.dup(stdchannel.fileno())
         dest_file = open(dest_filename, 'w')
@@ -93,37 +82,31 @@ def stdchannel_redirected(stdchannel, dest_filename):
         if dest_file is not None:
             dest_file.close()
 
-class LibraryChecker:
-    '''Container for libraries that checks their existence.
+class EnvironmentSniffer:
+    '''Check for availability of headers and libraries of FFTW and MPI.
+
+    :param compiler:
+
+        Distutils.ccompiler; The compiler should preferably be the compiler
+        that is used for actual compilation to ensure that include directories etc are identical.
 
     :param exclude:
 
         Iterable of strings; pass packages to ignore in here. Example: exclude=('DOUBLE_MPI', 'SINGLE_MPI', 'LONG_MPI', 'QUAD_MPI')
 
     '''
-    def __init__(self, exclude=None):
+    def __init__(self, compiler, exclude=None):
         self.include_dirs = [os.path.join(os.getcwd(), 'include'),
                              os.path.join(os.getcwd(), 'pyfftw'),
                              numpy.get_include()]
         self.libraries = []
         self.library_dirs = []
-        self.package_data = {} # why package data only updated for windows?
+        self.package_data = {} # todo why package data only updated for windows?
         self.compile_time_env = {}
 
-        # construct checks
-        self.data = {}
+        self.compiler = compiler
 
-        # now check library existence by linking. If the linker can't find it, we don't have it
-        # create compiler and customize with environment variables
-        self.compiler = customize_compiler(new_compiler())
-
-        # where compiler warnings go
-        if log._global_log.threshold == 0:
-            self.redirect = os.devnull
-        else:
-            self.redirect = sys.stdout
-
-        have_mpi_h = self.check_mpi()
+        have_mpi_h = self.has_header(['mpi.h'], include_dirs=self.include_dirs)
         if have_mpi_h:
             try:
                 import mpi4py
@@ -132,6 +115,8 @@ class LibraryChecker:
                 print("Could not import mpi4py. Skipping support for FFTW MPI.")
                 have_mpi_h = False
 
+        # construct checks
+        self.data = {}
         data_types = ['DOUBLE', 'SINGLE', 'LONG', 'QUAD']
         data_types_short = ['', 'f', 'l', 'q']
         lib_types = ['', '_THREADS', '_OMP']
@@ -165,7 +150,6 @@ class LibraryChecker:
             if exclude is not None and macro[5:] in exclude:
                 exists = False
             else:
-                # print macro, lib, function
                 exists = self.has_function(function, libraries=(lib,),
                                            include_dirs=self.include_dirs,
                                            library_dirs=self.library_dirs)
@@ -197,31 +181,27 @@ class LibraryChecker:
         if not have_fftw:
             raise LinkError("Could not find any of the FFTW libraries")
 
-    def check_mpi(self):
-        with stdchannel_redirected(sys.stderr, os.devnull):
-            exists = self.has_function(r'void hello', includes=('mpi.h',),
-                                       include_dirs=self.include_dirs,
-                                       library_dirs=self.library_dirs)
-        return exists
-
     def has_function(self, function, includes=None, libraries=None, include_dirs=None, library_dirs=None):
         '''Alternative implementation of distutils.ccompiler.has_function that deletes the output and works reliably.'''
 
         if includes is None:
             includes = []
         if libraries is None:
-            libraries = []
+            libraries = self.libraries
         if include_dirs is None:
-            include_dirs = []
+            include_dirs = self.include_dirs
         if library_dirs is None:
-            library_dirs = []
+            library_dirs = self.library_dirs
 
-        msg = "Checking for %s" % function
+        msg = "Checking"
+        if function:
+            msg += " for %s" % function
         if libraries:
             msg += " in " + str(libraries)
         if includes:
             msg += " with includes " + str(includes)
         msg += "..."
+        status = "no"
 
         import tempfile, shutil
 
@@ -233,7 +213,7 @@ class LibraryChecker:
                 for inc in includes:
                     f.write('#include <%s>\n' % inc)
                 if function:
-                    f.write('void %s();\n' % function)
+                    f.write('void %s(void);\n' % function)
                 # f.write('int main(void) {\n')
                 # f.write('    %s();\n' % function)
                 # f.write('}\n')
@@ -245,7 +225,7 @@ class LibraryChecker:
                 # output file is stored relative to input file since
                 # the output has the full directory, joining with the
                 # file root gives the right directory
-                with stdchannel_redirected(sys.stderr, self.redirect):
+                with stdchannel_redirected(sys.stdout, os.devnull), stdchannel_redirected(sys.stderr, os.devnull):
                     objects = self.compiler.compile([fname], output_dir=file_root, include_dirs=include_dirs)
             except CompileError:
                 return False
@@ -253,7 +233,7 @@ class LibraryChecker:
                 log.info(e)
                 return False
             try:
-                with stdchannel_redirected(sys.stderr, self.redirect):
+                with stdchannel_redirected(sys.stdout, os.devnull), stdchannel_redirected(sys.stderr, os.devnull):
                     # using link_executable, LDFLAGS that the user can modify are ignored
                     self.compiler.link_shared_object(objects,
                                                      os.path.join(tmpdir, 'a.out'),
@@ -262,26 +242,21 @@ class LibraryChecker:
             except LinkError:
                 return False
             except Exception as e:
-                print 'Linking failed'
                 print e
                 return False
             # no error, seems to work
-            log.info(msg + "ok")
+            status = "ok"
             return True
         finally:
-            # shutil.rmtree(tmpdir)
-            log.info(msg + "no")
+            shutil.rmtree(tmpdir)
+            log.info(msg + status)
 
     def has_header(self, headers, include_dirs=None):
         '''Check for existence and usability of header files by compiling a test file.'''
         return self.has_function(None, includes=headers, include_dirs=include_dirs)
 
-checker = LibraryChecker()
-log.debug(checker.compile_time_env)
-
 class custom_build_ext(build_ext):
     def finalize_options(self):
-
         build_ext.finalize_options(self)
 
         if self.compiler is None:
@@ -308,22 +283,43 @@ class custom_build_ext(build_ext):
 
             self.libraries = _libraries
 
+    def build_extensions(self):
+        '''Check for availability of fftw libraries before building the wrapper.
+
+        Do it here to make sure we use the exact same compiler for checking includes/linking as for building the libraries.'''
+        sniffer = EnvironmentSniffer(self.compiler)
+
+        # read out information and modify compiler
+
+        # define macros, that is which part of wrapper is built
+        self.cython_compile_time_env = sniffer.compile_time_env
+
+        # prepend automatically generated info to whatever the user specified
+        include_dirs = sniffer.include_dirs or []
+        if self.include_dirs is not None:
+            include_dirs += self.include_dirs
+        self.compiler.set_include_dirs(include_dirs)
+
+        libraries = sniffer.libraries or None
+        if self.libraries is not None:
+            libraries += self.libraries
+        self.compiler.set_libraries(libraries)
+
+        library_dirs = sniffer.library_dirs
+        if self.library_dirs is not None:
+            library_dirs += self.library_dirs
+        self.compiler.set_library_dirs(self.library_dirs)
+
+        # delegate actual work to standard implementation
+        build_ext.build_extensions(self)
+
 # recompile if any of these files are modified
 dependencies = [os.path.join('pyfftw', f) for f in ('pyfftw.pxd', 'mpi.pxd', 'mpi.pxi', 'utils.pxi')]
 
 ext_modules = [Extension('pyfftw.pyfftw',
                          sources=sources,
-                         libraries=checker.libraries,
-                         library_dirs=checker.library_dirs,
-                         include_dirs=checker.include_dirs,
                          extra_compile_args=['-Wno-maybe-uninitialized'],
                          depends=dependencies)]
-
-# TODO only use cythonize to pass macros in compile_time_env to the source compilation.
-# if we can do it with standard macros, try that!
-
-from Cython.Build import cythonize
-ext_modules = cythonize(ext_modules, compile_time_env=checker.compile_time_env)
 
 long_description = '''
 pyFFTW is a pythonic wrapper around `FFTW <http://www.fftw.org/>`_, the
@@ -394,10 +390,11 @@ setup_args = {
             ],
         'packages':['pyfftw', 'pyfftw.builders', 'pyfftw.interfaces'],
         'ext_modules': ext_modules,
-        'include_dirs': checker.include_dirs,
-        'package_data': checker.package_data,
+#         'include_dirs': checker.include_dirs,
+# todo package data from checker?
+#         'package_data': checker.package_data,
         'cmdclass': {'test': TestCommand,
-            'build_ext': custom_build_ext},
+                     'build_ext': custom_build_ext},
   }
 
 if __name__ == '__main__':
