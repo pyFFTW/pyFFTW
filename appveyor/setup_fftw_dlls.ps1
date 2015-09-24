6fccd80bf96b04ce0ac61cabe93192e4a8b61769
Henry Gomersall <heng@smartacoustics.co.uk>
2015-09-24 15:04:02 +0100

    An attempt to set the lib path explicitly for appveyor.

--- appveyor/setup_fftw_dlls.ps1
+++ appveyor/setup_fftw_dlls.ps1
@@ -13,17 +13,12 @@ function SetupFFTWDLLs(){
     $zip_destination = "fftw-3.3.4-dll.zip"
     Invoke-WebRequest $source_file -OutFile $zip_destination
 
-    iex "dir"
-    Write-Host $env:WIN_SDK_ROOT
-    Write-Host $env:WINDOWS_SDK_VERSION
-    iex "dir $env:WIN_SDK_ROOT\$env:WINDOWS_SDK_VERSION"
-
     iex "7z.exe e $zip_destination -opyfftw *.dll"    
     iex "7z.exe e $zip_destination -opyfftw *.def"
 
-    iex "lib /def:pyfftw\libfftw3-3.def"
-    iex "lib /def:pyfftw\libfftw3f-3.def"
-    iex "lib /def:pyfftw\libfftw3l-3.def"
+    iex "$env:WIN_SDK_ROOT\$env:WINDOWS_SDK_VERSION\Bin\lib.exe /def:pyfftw\libfftw3-3.def"
+    iex "$env:WIN_SDK_ROOT\$env:WINDOWS_SDK_VERSION\Bin\lib.exe /def:pyfftw\libfftw3f-3.def"
+    iex "$env:WIN_SDK_ROOT\$env:WINDOWS_SDK_VERSION\Bin\lib.exe /def:pyfftw\libfftw3l-3.def"
 
 }
 
