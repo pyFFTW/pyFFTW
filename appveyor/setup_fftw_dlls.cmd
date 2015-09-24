4634b66564ead80b0728a805dfa8f54334f22425
Henry Gomersall <heng@smartacoustics.co.uk>
2015-09-24 16:50:25 +0100

    Setup correct path to ps file.

--- appveyor/setup_fftw_dlls.cmd
+++ appveyor/setup_fftw_dlls.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
 "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
 "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd"
-call powershell setup_fftw_dlls.ps1
+call powershell -File appveyor\setup_fftw_dlls.ps1
