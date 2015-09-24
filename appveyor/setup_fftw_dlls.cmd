4422fed51a601a6860333751aad4443cd6c5a15d
Henry Gomersall <heng@smartacoustics.co.uk>
2015-09-24 16:42:05 +0100

    Added a explicit setting for the SDK version to use.

--- appveyor/setup_fftw_dlls.cmd
+++ appveyor/setup_fftw_dlls.cmd
@@ -1,3 +1,4 @@
 @ECHO OFF
+"%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
 "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd"
 call powershell setup_fftw_dlls.ps1 || EXIT 1
