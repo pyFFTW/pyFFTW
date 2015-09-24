3af896d8406b0b9c513239a3a34326f21deb4048
Henry Gomersall <heng@smartacoustics.co.uk>
2015-09-24 14:44:41 +0100

    Another bit of debugging info.

--- appveyor/setup_fftw_dlls.ps1
+++ appveyor/setup_fftw_dlls.ps1
@@ -10,12 +10,13 @@ function SetupFFTWDLLs(){
         $source_file = "ftp://ftp.fftw.org/pub/fftw/fftw-3.3.4-dll64.zip"
     }
 
-    $zip_destination = "\fftw-3.3.4-dll.zip"
+    $zip_destination = "fftw-3.3.4-dll.zip"
     Invoke-WebRequest $source_file -OutFile $zip_destination
 
     iex "dir"
     Write-Host $env:WIN_SDK_ROOT
-    Write-Host $env:WIN_SDK_VERSION
+    Write-Host $env:WINDOWS_SDK_VERSION
+    iex "dir $env:WIN_SDK_ROOT\$env:WINDOWS_SDK_VERSION"
 
     iex "7z.exe e $zip_destination -opyfftw *.dll"    
     iex "7z.exe e $zip_destination -opyfftw *.def"
