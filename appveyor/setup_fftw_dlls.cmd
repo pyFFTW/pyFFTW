ade23251de7e4d765f11ded78f6029a8b65fb18e
Henry Gomersall <heng@smartacoustics.co.uk>
2015-09-24 16:58:27 +0100

    moved everything inside .cmd file.

--- appveyor/setup_fftw_dlls.cmd
+++ appveyor/setup_fftw_dlls.cmd
@@ -1,4 +1,17 @@
 @ECHO OFF
 "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
 "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd"
-call powershell -File appveyor\setup_fftw_dlls.ps1
+
+IF %PYTHON_ARCH% == 64 (
+    appveyor DownloadFile "ftp://ftp.fftw.org/pub/fftw/fftw-3.3.4-dll64.zip"
+    SET FFTW_DLL_FILENAME=fftw-3.3.4-dll64.zip
+) ELSE (
+    appveyor DownloadFile "ftp://ftp.fftw.org/pub/fftw/fftw-3.3.4-dll32.zip"
+    SET FFTW_DLL_FILENAME=fftw-3.3.4-dll32.zip
+)
+
+call 7z.exe e %FFTW_DLL_FILENAME% -opyfftw *.dll
+call 7z.exe e %FFTW_DLL_FILENAME% -opyfftw *.def
+call lib /def:pyfftw\libfftw3-3.def
+call lib /def:pyfftw\libfftw3f-3.def
+call lib /def:pyfftw\libfftw3l-3.def
