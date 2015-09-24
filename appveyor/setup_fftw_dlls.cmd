846357c009da75aad5c4bcc64d0a6f2474bfed2f
Henry Gomersall <heng@smartacoustics.co.uk>
2015-09-24 17:03:02 +0100

    Added some debug output to the cmd file.

--- appveyor/setup_fftw_dlls.cmd
+++ appveyor/setup_fftw_dlls.cmd
@@ -2,6 +2,7 @@
 "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Setup\WindowsSdkVer.exe" -q -version:%WINDOWS_SDK_VERSION%
 "%WIN_SDK_ROOT%\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd"
 
+ECHO Downloading DLL files
 IF %PYTHON_ARCH% == 64 (
     appveyor DownloadFile "ftp://ftp.fftw.org/pub/fftw/fftw-3.3.4-dll64.zip"
     SET FFTW_DLL_FILENAME=fftw-3.3.4-dll64.zip
@@ -9,9 +10,12 @@ IF %PYTHON_ARCH% == 64 (
     appveyor DownloadFile "ftp://ftp.fftw.org/pub/fftw/fftw-3.3.4-dll32.zip"
     SET FFTW_DLL_FILENAME=fftw-3.3.4-dll32.zip
 )
-
+call ls pyfftw
+ECHO Extracting DLLs from %FFTW_DLL_FILENAME%
 call 7z.exe e %FFTW_DLL_FILENAME% -opyfftw *.dll
 call 7z.exe e %FFTW_DLL_FILENAME% -opyfftw *.def
+ECHO Generating def files
 call lib /def:pyfftw\libfftw3-3.def
 call lib /def:pyfftw\libfftw3f-3.def
 call lib /def:pyfftw\libfftw3l-3.def
+
