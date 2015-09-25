51511c96dca9ed8f40512e5fe143911d09d39448
Henry Gomersall <heng@smartacoustics.co.uk>
2015-09-25 09:45:45 +0100

    Fix typo.

--- appveyor/install.ps1
+++ appveyor/install.ps1
@@ -226,7 +226,6 @@ function InstallMinicondaPip ($python_home) {
 function main () {
     #InstallPython $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     #InstallPip $env:PYTHON
-    Write-Hose "Installing minicon
     InstallMiniconda $env:PYTHON_VERSION $env:PYTHON_ARCH $env:PYTHON
     InstallMinicondaPip $env:PYTHON
 
