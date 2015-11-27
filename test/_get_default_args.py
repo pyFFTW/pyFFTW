400a22662fd4cfa7822f5293ab642944cd85bc1c
Henry Gomersall <heng@smartacoustics.co.uk>
2015-11-27 12:09:08 +0000

    Removed the deprecated and slightly hacky use of inspect.getargspec for python 3.

---
+++ test/_get_default_args.py
@@ -0,0 +1,58 @@
+# Copyright 2015 Knowledge Economy Developments Ltd
+# 
+# Henry Gomersall
+# henry.gomersall@kedevelopments.co.uk
+#
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# * Neither the name of the copyright holder nor the names of its contributors
+# may be used to endorse or promote products derived from this software without
+# specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+#
+
+import sys
+import inspect
+
+def get_default_args(function):
+
+    if sys.version_info < (3, 0):
+        # The slightly hacky Python 2.7 way
+        argspec = inspect.getargspec(function)
+        default_args = dict(list(zip(
+            argspec.args[-len(argspec.defaults):], argspec.defaults)))
+
+    else:
+        # The better Python 3 way
+        sig = inspect.signature(function)
+
+        default_args = {}
+        for parameter in sig.parameters:
+
+            default_val = sig.parameters[parameter].default
+            if default_val is not inspect.Parameter.empty:
+                # Add it to the parameter list
+                default_args[parameter] = default_val
+
+    return default_args
