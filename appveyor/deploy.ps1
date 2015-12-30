c86dc2747f176546c0a21246c735510ee0892154
Henry Gomersall <heng@smartacoustics.co.uk>
2015-12-30 12:16:35 +0000

    tweak to deploy.ps1

--- appveyor/deploy.ps1
+++ appveyor/deploy.ps1
@@ -1,7 +1,7 @@
 
 function deploy_to_bintray() {
     iex "activate build_env"
-    iex "conda install numpy"
+    #iex "conda install numpy"
     $version_string = (iex "python -m pyfftw.version") | Out-String
     $version_list = $version_string.Split("`r`n")
     $short_version = [string]$version_list[0]
@@ -14,12 +14,12 @@ function deploy_to_bintray() {
     }
 
     $python_version = $env:PYTHON_VERSION -replace '\.',''
-
     $filename = "pyFFTW-$version-cp$python_version-none-win_$platform_suffix.whl"
     $filepath = ".\dist\$filename"
     $username_password = "$env:bintray_username:$env:bintray_api_key"
 
-    iex "curl.exe -T$filepath -u$username_password https://api.bintray.com/content/hgomersall/generic/PyFFTW-development-builds/$short_version/$filename"
+    Write-Host "$filepath https://api.bintray.com/content/hgomersall/generic/PyFFTW-development-builds/$short_version/$filename"
+    iex "curl.exe -T $filepath -u$username_password https://api.bintray.com/content/hgomersall/generic/PyFFTW-development-builds/$short_version/$filename"
 }
 
 function deploy_to_pypi () {
