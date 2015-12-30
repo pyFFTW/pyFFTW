9fc19d7e455bdab02b5e699b6171080630409b36
Henry Gomersall <heng@smartacoustics.co.uk>
2015-12-30 13:12:21 +0000

    updated for 32 bit wheels.

--- appveyor/deploy.ps1
+++ appveyor/deploy.ps1
@@ -7,13 +7,13 @@ function deploy_to_bintray() {
     $version = [string]$version_list[2]
 
     if ($env:PYTHON_ARCH -eq "32") {
-        $platform_suffix = "x86"
+        $platform_suffix = "win32"
     } else {
-        $platform_suffix = "amd64"
+        $platform_suffix = "win_amd64"
     }
 
     $python_version = $env:PYTHON_VERSION -replace '\.',''
-    $filename = "pyFFTW-$version-cp$python_version-none-win_$platform_suffix.whl"
+    $filename = "pyFFTW-$version-cp$python_version-none-$platform_suffix.whl"
     $filepath = ".\dist\$filename"
     $username_password = "${env:bintray_username}:${env:bintray_api_key}"
 
