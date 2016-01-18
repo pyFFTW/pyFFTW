91b306ceb7b6bd6d6c525bc5a3853d9be72a2509
Zachary Pincus <zpincus@gmail.com>
2016-01-18 00:02:40 -0600

    Run test without creating copies
    Forcibly use the exact same arrays (without copies) throughout
    FFTW_WISDOM_ONLY testing to make sure alignment of the arrays does not
    change.

--- test/test_pyfftw_complex.py
+++ test/test_pyfftw_complex.py
@@ -198,22 +198,21 @@ class Complex64FFTW1DTest(object):
         a, b = self.create_test_arrays(in_shape, out_shape)
         forget_wisdom()
         # with no wisdom, an error should be raised with FFTW_WISDOM_ONLY
-        # (NB: wisdom is specific to aligned/unaligned distinction, so we
-        # ensure that the arrays are always treated as unaligned. Internally, 
-        # run_validate_fft copies the input arrays, which can randomly
-        # cause them to be aligned or unaligned. Without forcing unaligned, this
-        # could otherwise make the wisdom invalid for future planning...
+        # 
+        # NB: wisdom is specific to aligned/unaligned distinction, so we must
+        # ensure that the arrays don't get copied (and potentially
+        # switched between aligned and unaligned) by run_validate_fft()... 
         self.assertRaisesRegex(RuntimeError, 'No FFTW wisdom', 
                 self.run_validate_fft, *(a, b, axes), 
                 **{'flags':('FFTW_ESTIMATE', 'FFTW_WISDOM_ONLY'), 
-                   'force_unaligned_data': True})
+                   'create_array_copies': False})
         # now plan the FFT
         self.run_validate_fft(a, b, axes, flags=('FFTW_ESTIMATE',),
-                force_unaligned_data=True)
+                create_array_copies=False)
         # now FFTW_WISDOM_ONLY should not raise an error because the plan should
         # be in the wisdom
         self.run_validate_fft(a, b, axes, flags=('FFTW_ESTIMATE', 
-                'FFTW_WISDOM_ONLY'), force_unaligned_data=True)
+                'FFTW_WISDOM_ONLY'), create_array_copies=False)
 
     def test_destroy_input(self):
         '''Test the destroy input flag
