6b09f62d0418efb73593942ba6c19bc8ee013911
David Wells <drwells@vt.edu>
2016-04-10 23:15:24 -0400

    Fix fftpack vs fftw tests.
    Add another real to real normalization test.
    
    Skip some tests if scipy is not installed.
    
    Most of the binding tests require scipy.
    
    Undo the last commit.
    
    SciPy only supports three.
    
    Overhaul the scipy tests.
    
    np -> numpy
    
    Match the convention elsewhere in the test suite.
    
    Clean up the real to real tests.
    
    They now conform, more or less, to the rest of the tests in terms of
    their usage of infrastructure.
    
    Get rid of print statements.
    
    Convert some unused verification code into a test.
    
    This test verifies that all of the lookup tables (which are used through
    out the real to real tests) to the right thing.

--- test/test_r2r.py
+++ test/test_r2r.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
 #
 # Copyright 2014 Knowledge Economy Developments Ltd
-# Copyright 2014 David Wells
+# Copyright 2014 - 2016 David Wells
 #
 # Henry Gomersall
 # heng@kedevelopments.co.uk
@@ -40,8 +39,10 @@ from __future__ import division
 import itertools as it
 import random as rand
 import unittest
-import numpy as np
+import numpy
+
 import pyfftw
+from .test_pyfftw_base import run_test_suites
 
 discrete_sine_directions = ['FFTW_RODFT00', 'FFTW_RODFT01', 'FFTW_RODFT10',
                             'FFTW_RODFT11']
@@ -74,67 +75,58 @@ inverse_lookup = {
 }
 
 interpolated_function_lookup = {
-    'FFTW_RODFT00': lambda k, x: np.sin(np.pi*(k + 1)*x),
-    'FFTW_RODFT01': lambda k, x: np.sin(np.pi*(k + 0.5)*x),
-    'FFTW_RODFT10': lambda k, x: np.sin(np.pi*(k + 1)*x),
-    'FFTW_RODFT11': lambda k, x: np.sin(np.pi*(k + 0.5)*x),
+    'FFTW_RODFT00': lambda k, x: numpy.sin(numpy.pi*(k + 1)*x),
+    'FFTW_RODFT01': lambda k, x: numpy.sin(numpy.pi*(k + 0.5)*x),
+    'FFTW_RODFT10': lambda k, x: numpy.sin(numpy.pi*(k + 1)*x),
+    'FFTW_RODFT11': lambda k, x: numpy.sin(numpy.pi*(k + 0.5)*x),
 
-    'FFTW_REDFT00': lambda k, x: np.cos(np.pi*k*x),
-    'FFTW_REDFT10': lambda k, x: np.cos(np.pi*k*x),
-    'FFTW_REDFT01': lambda k, x: np.cos(np.pi*(k + 0.5)*x),
-    'FFTW_REDFT11': lambda k, x: np.cos(np.pi*(k + 0.5)*x),
+    'FFTW_REDFT00': lambda k, x: numpy.cos(numpy.pi*k*x),
+    'FFTW_REDFT10': lambda k, x: numpy.cos(numpy.pi*k*x),
+    'FFTW_REDFT01': lambda k, x: numpy.cos(numpy.pi*(k + 0.5)*x),
+    'FFTW_REDFT11': lambda k, x: numpy.cos(numpy.pi*(k + 0.5)*x),
 }
 
 nodes_lookup = {
-    'FFTW_RODFT00': lambda n: np.arange(n + 2)[1:-1]/(n + 1),
-    'FFTW_RODFT01': lambda n: np.arange(1, n + 1)/n,
-    'FFTW_RODFT10': lambda n: (np.arange(n) + 0.5)/n,
-    'FFTW_RODFT11': lambda n: (np.arange(n) + 0.5)/n,
+    'FFTW_RODFT00': lambda n: numpy.arange(n + 2)[1:-1]/(n + 1),
+    'FFTW_RODFT01': lambda n: numpy.arange(1, n + 1)/n,
+    'FFTW_RODFT10': lambda n: (numpy.arange(n) + 0.5)/n,
+    'FFTW_RODFT11': lambda n: (numpy.arange(n) + 0.5)/n,
 
-    'FFTW_REDFT00': lambda n: np.arange(n)/(n - 1),
-    'FFTW_REDFT10': lambda n: (np.arange(n) + 0.5)/n,
-    'FFTW_REDFT01': lambda n: np.arange(n)/n,
-    'FFTW_REDFT11': lambda n: (np.arange(n) + 0.5)/n,
+    'FFTW_REDFT00': lambda n: numpy.arange(n)/(n - 1),
+    'FFTW_REDFT10': lambda n: (numpy.arange(n) + 0.5)/n,
+    'FFTW_REDFT01': lambda n: numpy.arange(n)/n,
+    'FFTW_REDFT11': lambda n: (numpy.arange(n) + 0.5)/n,
 }
 
-class TestRandomRealTransforms(unittest.TestCase):
-    def test_normalisation(self):
-        for _ in range(50):
-            testcase = random_testcase()
-            self.assertTrue(testcase.test_normalisation())
-
-    def test_exact_data(self):
-        for _ in range(50):
-            testcase = random_testcase()
-            self.assertTrue(testcase.test_against_exact_data())
-
-    def test_random_data(self):
-        for _ in range(50):
-            testcase = random_testcase()
-            self.assertTrue(testcase.test_against_random_data())
-
-def test_lookups():
-    """Test that the lookup tables correctly pair node choices and
+class TestRealToRealLookups(unittest.TestCase):
+    '''Test that the lookup tables correctly pair node choices and
     function choices for using the DCT/DST as interpolators.
-    """
-    n = rand.randint(10, 20)
-    j = rand.randint(5, n) - 3
-    for transform in real_transforms:
-        nodes   = nodes_lookup[transform](n)
-        data    = interpolated_function_lookup[transform](j, nodes)
-        output  = np.empty_like(data)
-        plan    = pyfftw.FFTW(data, output, direction=[transform])
-        data[:] = interpolated_function_lookup[transform](j, nodes)
-        plan.execute()
-        tol = 4*j*n*1e-16
-        if transform == 'FFTW_RODFT00':
-            assert abs(output[j] - n - 1) < tol
-        elif transform == 'FFTW_REDFT00':
-            assert abs(output[j] - n + 1) < tol
-        else:
-            assert abs(output[j] - n) < tol
+    '''
+    def test_lookups(self):
+        n = rand.randint(10, 20)
+        j = rand.randint(5, n) - 3
+        for transform in real_transforms:
+            nodes   = nodes_lookup[transform](n)
+            data    = interpolated_function_lookup[transform](j, nodes)
+            output  = numpy.empty_like(data)
+            plan    = pyfftw.FFTW(data, output, direction=[transform])
+            data[:] = interpolated_function_lookup[transform](j, nodes)
+            plan.execute()
+            tol = 4*j*n*1e-16
+            if transform == 'FFTW_RODFT00':
+                self.assertTrue(abs(output[j] - n - 1) < tol)
+            elif transform == 'FFTW_REDFT00':
+                self.assertTrue(abs(output[j] - n + 1) < tol)
+            else:
+                self.assertTrue(abs(output[j] - n) < tol)
 
 class TestRealTransform(object):
+    '''Common set of functionality for performing tests on the real to
+    real transforms. This is not implemented as a distinct test class
+    (inheriting from unittest.TestCase) because its `__init__` method
+    takes multiple arguments as input which set up the size and
+    directions of the transform.
+    '''
     def __init__(self, directions, dims, axes=None, noncontiguous=True):
         """
         Arguments:
@@ -173,8 +165,8 @@ class TestRealTransform(object):
             self._input_array = empty_noncontiguous(dims)
             self._output_array = empty_noncontiguous(dims)
         else:
-            self._input_array = np.zeros(dims)
-            self._output_array = np.zeros(dims)
+            self._input_array = numpy.zeros(dims)
+            self._output_array = numpy.zeros(dims)
         self.plan = pyfftw.FFTW(self._input_array, self._output_array,
             axes=self.axes, direction=self.directions)
         self.inverse_plan = pyfftw.FFTW(self._input_array, self._output_array,
@@ -184,19 +176,19 @@ class TestRealTransform(object):
         return self._normalisation_factor == float(self.plan._get_N())
 
     def test_against_random_data(self):
-        data = np.random.rand(*self.dims)
+        data = numpy.random.rand(*self.dims)
         self._input_array[:] = data
         self.plan.execute()
         self._input_array[:] = self._output_array[:]
         self.inverse_plan.execute()
 
         data *= self._normalisation_factor
-        err = np.mean(np.abs(data - self._output_array))/self._normalisation_factor
+        err = numpy.mean(numpy.abs(data - self._output_array))/self._normalisation_factor
         return err < 10e-8
 
     def test_against_exact_data(self):
         points = grid(self.dims, self.axes, self.directions)
-        data   = np.ones_like(points[0])
+        data   = numpy.ones_like(points[0])
         wavenumbers = list()
         factors = list()
 
@@ -215,24 +207,24 @@ class TestRealTransform(object):
                 self.directions[index]]
             data *= sum((factor*interpolated_function(wavenumber, points[axis])
                          for factor, wavenumber in zip(_factors, _wavenumbers)))
-            wavenumbers.append(np.array(_wavenumbers))
-            factors.append(np.array(_factors))
+            wavenumbers.append(numpy.array(_wavenumbers))
+            factors.append(numpy.array(_factors))
 
         self._input_array[:] = data
         self.plan.execute()
 
         # zero all of the entries that do not correspond to a wavenumber.
-        exact_coefficients = np.ones(data.shape)
+        exact_coefficients = numpy.ones(data.shape)
         for index, axis in enumerate(self.axes):
             dim = self.dims[axis]
             sp = list(it.repeat(slice(None), len(data.shape)))
-            zero_indicies = (np.array(list(set(np.arange(0, dim))
+            zero_indicies = (numpy.array(list(set(numpy.arange(0, dim))
                                       - set(wavenumbers[index]))))
             if len(zero_indicies) == 0:
                 pass
             else:
                 sp[axis] = zero_indicies
-                mask = np.ones(data.shape)
+                mask = numpy.ones(data.shape)
                 mask[sp] = 0.0
                 exact_coefficients *= mask
 
@@ -244,9 +236,59 @@ class TestRealTransform(object):
                 sp[axis] = wavenumber
                 exact_coefficients[sp] *= factor
 
-        error = np.mean(np.abs(self._output_array/normalisation
+        error = numpy.mean(numpy.abs(self._output_array/normalisation
                               - exact_coefficients))
         return error < 1e-8
+
+
+def meshgrid(*x):
+    if len(x) == 1:
+        # necessary for one-dimensional case to work correctly. x is a
+        # tuple due to the * operator.
+        return x
+    else:
+        args = numpy.atleast_1d(*x)
+        s0 = (1,)*len(args)
+        return list(map(numpy.squeeze,
+                        numpy.broadcast_arrays(*[x.reshape(s0[:i] + (-1,) + s0[i + 1::])
+                                              for i, x in enumerate(args)])))
+
+
+def grid(shape, axes, directions, aspect_ratio=None):
+    grids = [numpy.linspace(1, 2, dim) for dim in shape]
+    for index, (axis, direction) in enumerate(zip(axes, directions)):
+        grids[axis] = nodes_lookup[direction](shape[axes[index]])
+
+    return numpy.array(meshgrid(*grids))
+
+
+def empty_noncontiguous(shape):
+    '''Create a non-contiguous empty array with shape `shape`.
+    '''
+    offsets = lambda s: [rand.randint(0, 3) for _ in s]
+    strides = lambda s: [rand.randint(1, 3) for _ in s]
+    parent_left_offsets = offsets(shape)
+    parent_right_offsets = offsets(shape)
+    parent_strides = strides(shape)
+
+    parent_shape = list()
+    child_slice = list()
+    for index, length in enumerate(shape):
+        left_offset = parent_left_offsets[index]
+        right_offset = parent_right_offsets[index]
+        stride = parent_strides[index]
+        parent_shape.append(left_offset + stride*length + right_offset)
+        if right_offset == 0:
+            child_slice.append(slice(left_offset, None, stride))
+        else:
+            child_slice.append(slice(left_offset, -1*right_offset, stride))
+
+    child = numpy.empty(parent_shape)[child_slice]
+    if list(child.shape) != list(shape):
+        raise ValueError("The shape of the noncontiguous array is incorrect."
+                         " This is a bug.")
+    return child
+
 
 def random_testcase():
     ndims = rand.randint(1, 5)
@@ -270,51 +312,29 @@ def random_testcase():
     else:
         return TestRealTransform(directions, dims, axes=axes)
 
-def meshgrid(*x):
-    if len(x) == 1:
-        # necessary for one-dimensional case to work correctly. x is a
-        # tuple due to the * operator.
-        return x
-    else:
-        args = np.atleast_1d(*x)
-        s0 = (1,)*len(args)
-        return list(map(np.squeeze,
-                        np.broadcast_arrays(*[x.reshape(s0[:i] + (-1,) + s0[i + 1::])
-                                              for i, x in enumerate(args)])))
 
-def grid(shape, axes, directions, aspect_ratio=None):
-    grids = [np.linspace(1, 2, dim) for dim in shape]
-    for index, (axis, direction) in enumerate(zip(axes, directions)):
-        grids[axis] = nodes_lookup[direction](shape[axes[index]])
+class RealToRealNormalisation(unittest.TestCase):
+    def test_normalisation(self):
+        for _ in range(50):
+            testcase = random_testcase()
+            self.assertTrue(testcase.test_normalisation())
 
-    return np.array(meshgrid(*grids))
+class RealToRealExactData(unittest.TestCase):
+    def test_exact_data(self):
+        for _ in range(50):
+            testcase = random_testcase()
+            self.assertTrue(testcase.test_against_exact_data())
 
-def empty_noncontiguous(shape):
-    """Create a non-contiguous empty array with shape `shape`.
-    """
-    offsets = lambda s: [rand.randint(0, 3) for _ in s]
-    strides = lambda s: [rand.randint(1, 3) for _ in s]
-    parent_left_offsets = offsets(shape)
-    parent_right_offsets = offsets(shape)
-    parent_strides = strides(shape)
+class RealToRealRandomData(unittest.TestCase):
+    def test_random_data(self):
+        for _ in range(50):
+            testcase = random_testcase()
+            self.assertTrue(testcase.test_against_random_data())
 
-    parent_shape = list()
-    child_slice = list()
-    for index, length in enumerate(shape):
-        left_offset = parent_left_offsets[index]
-        right_offset = parent_right_offsets[index]
-        stride = parent_strides[index]
-        parent_shape.append(left_offset + stride*length + right_offset)
-        if right_offset == 0:
-            child_slice.append(slice(left_offset, None, stride))
-        else:
-            child_slice.append(slice(left_offset, -1*right_offset, stride))
-
-    child = np.empty(parent_shape)[child_slice]
-    if list(child.shape) != list(shape):
-        raise ValueError("The shape of the noncontiguous array is incorrect."
-                         " This is a bug.")
-    return child
+test_cases = (TestRealToRealLookups,
+              RealToRealNormalisation,
+              RealToRealExactData,
+              RealToRealRandomData,)
 
 if __name__ == '__main__':
-    unittest.main()
+    run_test_suites(test_cases)
