b8c4dc88a595e7309ec084273a908057b663f433
John Kirkham <kirkhamj@janelia.hhmi.org>
2018-03-13 15:43:53 -0400

    MNT: Include Dask interface only for NumPy 1.10+
    As the Dask versions we are interested in are not really compatible with
    any NumPy before 1.10+ and can sometimes fail (even on import) in
    bizarre ways, simply add a version check to ensure that NumPy 1.10+ is
    installed before even trying to import Dask. Make sure to clean up
    everything imported whether or not the Dask interface can be added
    independently of this NumPy version check.

--- pyfftw/interfaces/__init__.py
+++ pyfftw/interfaces/__init__.py
@@ -236,10 +236,18 @@ else:
     del scipy
     from . import scipy_fftpack
 
-try:
-    from dask.array.fft import fft_wrap
-except ImportError:
-    pass
-else:
-    del fft_wrap
+from distutils.version import LooseVersion
+import numpy
+
+fft_wrap = None
+if LooseVersion(numpy.__version__) > LooseVersion("1.10"):
+    try:
+        from dask.array.fft import fft_wrap
+    except ImportError:
+        pass
+del LooseVersion
+del numpy
+
+if fft_wrap:
     from . import dask_fft
+del fft_wrap
