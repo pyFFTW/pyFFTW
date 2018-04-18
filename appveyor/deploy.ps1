34dd1aa2a1444a45d4ca445c675fad51fc850c77
Matthieu Melot <matthieu.melot@andritz.com>
2018-04-18 09:27:09 -0400

    MNT: Use short version string for bintray upload

--- appveyor/deploy.ps1
+++ appveyor/deploy.ps1
@@ -2,6 +2,7 @@
 function deploy_to_bintray() {
     $version_string = (iex "python -c `"import pyfftw; print(pyfftw.__version__)`"") | Out-String
     $version = $version_string -replace "`t|`n|`r",""
+    $short_version = [string]$version.Split("+")[0]
 
     if ($env:PYTHON_ARCH -eq "32") {
         $platform_suffix = "win32"
@@ -15,8 +16,8 @@ function deploy_to_bintray() {
     $username_password = "${env:bintray_username}:${env:bintray_api_key}"
 
     Write-Host "Uploading: $filepath"
-    Write-Host "to https://api.bintray.com/content/hgomersall/generic/PyFFTW-development-builds/$version/$filename"
-    iex "curl.exe -s -T $filepath -u$username_password -H `"X-Bintray-Package:PyFFTW-development-builds`" -H `"X-Bintray-Version:$version`" -H `"X-Bintray-Publish: 1`" -H `"X-Bintray-Override: 1`" https://api.bintray.com/content/hgomersall/generic/$filename"
+    Write-Host "to https://api.bintray.com/content/hgomersall/generic/PyFFTW-development-builds/$short_version/$filename"
+    iex "curl.exe -s -T $filepath -u$username_password -H `"X-Bintray-Package:PyFFTW-development-builds`" -H `"X-Bintray-Version:$short_version`" -H `"X-Bintray-Publish: 1`" -H `"X-Bintray-Override: 1`" https://api.bintray.com/content/hgomersall/generic/$filename"
 }
 
 function deploy_to_pypi () {
