d51969623505951fc1a6eb3e15de45429416caf9
Gabriel Fougeron <gabriel.fougeron@esi-group.com>
2024-04-02 16:33:02 +0200

    Adapted legal text in test_pyfftw_nogil

--- tests/test_pyfftw_nogil.py
+++ tests/test_pyfftw_nogil.py
@@ -1,10 +1,3 @@
-# Copyright 2014 Knowledge Economy Developments Ltd
-#
-# Henry Gomersall
-# heng@kedevelopments.co.uk
-#
-# All rights reserved.
-#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
